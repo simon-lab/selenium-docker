b74ff0b91645212303baab0122381e3e047efc85
Raja Simon <rajasimon0@gmail.com>
2025-03-13 10:30:40 +0700

    // bat

--- runner.bat
+++ runner.bat
@@ -1,44 +1,44 @@
-#!/bin/bash
+@REM #!/bin/bat
 
-#-------------------------------------------------------------------
-#  This script expects the following environment variables
-#     HUB_HOST
-#     BROWSER
-#     THREAD_COUNT
-#     TEST_SUITE
-#-------------------------------------------------------------------
+@REM #-------------------------------------------------------------------
+@REM #  This script expects the following environment variables
+@REM #     HUB_HOST
+@REM #     BROWSER
+@REM #     THREAD_COUNT
+@REM #     TEST_SUITE
+@REM #-------------------------------------------------------------------
 
-# Let's print what we have received
-echo "-------------------------------------------"
-echo "HUB_HOST      : ${HUB_HOST:-hub}"
-echo "BROWSER       : ${BROWSER:-chrome}"
-echo "THREAD_COUNT  : ${THREAD_COUNT:-1}"
-echo "TEST_SUITE    : ${TEST_SUITE}"
-echo "-------------------------------------------"
+@REM # Let's print what we have received
+@REM echo "-------------------------------------------"
+@REM echo "HUB_HOST      : ${HUB_HOST:-hub}"
+@REM echo "BROWSER       : ${BROWSER:-chrome}"
+@REM echo "THREAD_COUNT  : ${THREAD_COUNT:-1}"
+@REM echo "TEST_SUITE    : ${TEST_SUITE}"
+@REM echo "-------------------------------------------"
 
-# Do not start the tests immediately. Hub has to be ready with browser nodes
-echo "Checking if hub is ready..!"
-count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
-  count=$((count+1))
-  echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
-  then
-      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-      exit 1
-  fi
-  sleep 1
-done
+@REM # Do not start the tests immediately. Hub has to be ready with browser nodes
+@REM echo "Checking if hub is ready..!"
+@REM count=0
+@REM while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
+@REM do
+@REM   count=$((count+1))
+@REM   echo "Attempt: ${count}"
+@REM   if [ "$count" -ge 30 ]
+@REM   then
+@REM       echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+@REM       exit 1
+@REM   fi
+@REM   sleep 1
+@REM done
 
-# At this point, selenium grid should be up!
-echo "Selenium Grid is up and running. Running the test...."
+@REM # At this point, selenium grid should be up!
+@REM echo "Selenium Grid is up and running. Running the test...."
 
-# Start the java command
-java -cp 'libs/*' \
-     -Dselenium.grid.enabled=true \
-     -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
-     -Dbrowser="${BROWSER:-chrome}" \
-     org.testng.TestNG \
-     -threadcount "${THREAD_COUNT:-1}" \
-     test-suites/"${TEST_SUITE}"
+@REM # Start the java command
+@REM java -cp 'libs/*' \
+@REM      -Dselenium.grid.enabled=true \
+@REM      -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
+@REM      -Dbrowser="${BROWSER:-chrome}" \
+@REM      org.testng.TestNG \
+@REM      -threadcount "${THREAD_COUNT:-1}" \
+@REM      test-suites/"${TEST_SUITE}"
